30b4ff88c3277317eddd6990862244f13b97580d
JH <jhenrique.vissotto@gmail.com>
2025-03-02 01:27:01 -0300

    Delete TScript - Frontend - Angular directory

--- TScript - Frontend - Angular/src/app/pages/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './list/list.component'
-export * from './counter/counter.component'
-export * from './counter2/counter2.component'
-export * from './counter3/counter3.component'
-export * from './api/api.component'
--- TScript - Frontend - Angular/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * as Api from './api'
-export * as GlobalState from './global-state'
