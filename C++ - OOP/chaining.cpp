5f140d5c0a71c9bf36a47a647a8d844056b92800
JH <jhenrique.vissotto@gmail.com>
2025-04-17 20:42:54 -0700

    Add files via upload

---
+++ C++ - OOP/chaining.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+class Counter {
+    public: 
+        int ini;
+        int val;
+
+    Counter(int ini = 0): 
+        ini(ini), 
+        val(ini) {}
+
+    Counter& set(int _new)  { val = _new;   return *this; }
+    Counter& reset()        { val =  ini;   return *this; }
+    Counter& increment()    { val +=   1;   return *this; }
+    Counter& decrement()    { val -=   1;   return *this; }
+    Counter& add(int by)    { val +=  by;   return *this; }
+    Counter& sub(int by)    { val -=  by;   return *this; }
+    int      get()          {               return   val; }
+};
+
+
+int main() {
+    Counter counter;
+    int value = counter.add(100).sub(10).increment().decrement().get();
+    cout << "value: " << value << endl;
+    return 0;
+}
