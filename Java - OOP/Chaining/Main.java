0652c194432c6bf021862d8dabbdd598b7765c89
JH <jhenrique.vissotto@gmail.com>
2025-04-18 09:56:17 -0700

    Delete Java - OOP/Chaining directory

--- Java - OOP/Chaining/Main.java
+++
@@ -1,9 +0,0 @@
-package Chaining;
-
-public class Main {
-    public static void main(String[] args) {
-        Counter counter = new Counter();
-        int value = counter.add(100).sub(10).increment().decrement().get();
-        System.out.println("value: " + value);
-    }
-}
