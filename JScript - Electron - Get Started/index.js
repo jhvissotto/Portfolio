dce69c2c89a94b4fafb939cc12c1fa850e00e47c
JH <jhenrique.vissotto@gmail.com>
2025-04-18 22:23:07 -0700

    Add files via upload

---
+++ JScript - Electron - Get Started/index.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    win.loadURL('data:text/html,<h1>Greetings!</h1>');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
