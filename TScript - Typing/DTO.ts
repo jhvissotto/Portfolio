8767a7cade84fddda9ccc50e8a234fe444400ce0
JH <jhenrique.vissotto@gmail.com>
2025-04-22 20:56:50 -0700

    Add files via upload

---
+++ TScript - Typing/DTO.ts
@@ -0,0 +1,13 @@
+type DTO = {
+    bool:   boolean
+    num:    number
+    text:   string
+    enum:   'AAA'|'BBB'|'CCC'
+    time:   Date    
+}
+
+const data: DTO[] = [
+    {  bool:true,  num:123,  text:'text',  enum:'AAA',  time: new Date() }, 
+    {  bool:true,  num:456,  text:'text',  enum:'AAA',  time: new Date() }, 
+    {  bool:true,  num:789,  text:'text',  enum:'AAA',  time: new Date() }, 
+]
