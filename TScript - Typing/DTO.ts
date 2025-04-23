c11f4d22ce9156c4ac64c55943c080a1965f0bc1
JH <jhenrique.vissotto@gmail.com>
2025-04-22 20:57:50 -0700

    Update DTO.ts

--- TScript - Typing/DTO.ts
+++ TScript - Typing/DTO.ts
@@ -8,6 +8,6 @@ type DTO = {
 
 const data: DTO[] = [
     {  bool:true,  num:123,  text:'text',  enum:'AAA',  time: new Date() }, 
-    {  bool:true,  num:456,  text:'text',  enum:'AAA',  time: new Date() }, 
-    {  bool:true,  num:789,  text:'text',  enum:'AAA',  time: new Date() }, 
+    {  bool:true,  num:456,  text:'text',  enum:'BBB',  time: new Date() }, 
+    {  bool:true,  num:789,  text:'text',  enum:'CCC',  time: new Date() }, 
 ]
