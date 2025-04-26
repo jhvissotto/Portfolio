591aa6336cc4159e29cca6f0b74e47c69bf9c8aa
JH <jhenrique.vissotto@gmail.com>
2025-04-26 13:53:24 -0700

    Add files via upload

---
+++ C++ - Get Started 1/json.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <nlohmann/json.hpp> 
+
+using namespace std;
+using json = nlohmann::json;
+
+
+int main() {
+
+    struct Item {
+        bool*   bool_value;
+        double  value;
+        string  text;
+    };
+
+    string A = json::array({
+        {{"bool",nullptr}, {"value",nan("")}, {"text","AAA"}},
+        {{"bool",false},   {"value",123},     {"text","BBB"}},
+        {{"bool",true},    {"value",456},     {"text","CCC"}}
+    }).dump();  
+
+    json B = json::parse(R"([{"bool":true,"value":123,"text":"AAA"},{"bool":false,"value":456,"text":"BBB"},{"bool":null,"value":789,"text":"CCC"}])");
+
+    return 0;
+}
