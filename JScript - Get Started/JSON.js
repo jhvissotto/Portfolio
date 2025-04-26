8458947fb2db923c72087b70838ae41dc4c3670c
JH <jhenrique.vissotto@gmail.com>
2025-04-26 13:47:31 -0700

    Update JSON.js

--- JScript - Get Started/JSON.js
+++ JScript - Get Started/JSON.js
@@ -1,11 +1,11 @@
 const A = JSON.stringify([
-    { bool:null,  value:123, text:'AAA' },
-    { bool:false, value:456, text:'BBB' },
-    { bool:true,  value:789, text:'CCC' },
+    { bool:null,  value:NaN, text:'AAA' },
+    { bool:false, value:123, text:'BBB' },
+    { bool:true,  value:456, text:'CCC' },
 ])
 
 console.log({ A })
 
-const B = JSON.parse('[{"bool":true,"value":123,"text":"AAA"},{"bool":false,"value":456,"text":"BBB"},{"bool":null,"value":789,"text":"CCC"}]')
+const B = JSON.parse('[{"bool":null,"value":null,"text":"AAA"},{"bool":false,"value":123,"text":"BBB"},{"bool":true,"value":456,"text":"CCC"}]')
 
 console.log({ B })
